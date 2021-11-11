8b611f231fc9cfa13961e9286b2b199802e5655a
LEFDAOUI-Wissal <72799001+LEFDAOUI-Wissal@users.noreply.github.com>
2021-11-11 11:44:53 +0100

    Delete tp2.h

--- tp2.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-
-float Dichotomie(float(*f)(float), float a, float b, int n);
-float Dichotomie2(float(*f)(float), float a, float b, float eps);
-float Newton(float (*f)(float), float (*g)(float), float a0, int n);
